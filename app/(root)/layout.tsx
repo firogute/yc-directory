faf96be35e3a0f58a14fc2cb7e031eacfd468463
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-12 10:51:23 +0300

    reorganize component imports and enhance layout with SearchForm

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,5 @@
 import { ReactNode } from "react";
-import NavBar from "../components/NavBar";
+import NavBar from "../../components/NavBar";
 
 export default function layout({
   children,
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,28 @@
 import Image from "next/image";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const query = (await searchParams).query;
+  // console.log(query);
   return (
     <>
-      <section className="pink_container"> </section>
-      <h1 className="heading">Home</h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup,
+          <br /> Connect with Entrepreneurs{" "}
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
     </>
   );
 }
