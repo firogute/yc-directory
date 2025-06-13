605ee7b5315e017fad26d1561cd57ef2a2b3668f
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-13 07:19:02 +0300

    add startup view tracking with ppr, enhance detail display, and implement markdown rendering

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,7 +1,15 @@
+import React, { Suspense } from "react";
+import { formatDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
 import { STARTUP_BY_ID_QUERY } from "@/sanity/lib/queries";
 import { notFound } from "next/navigation";
-import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import markdownit from "markdown-it";
+import { Skeleton } from "@/components/ui/skeleton";
+import View from "@/components/View";
+
+const md = markdownit();
 
 export const experimental_ppr = true;
 
@@ -11,9 +19,67 @@ const page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
 
   if (!post) return notFound();
+
+  const {
+    _createdAt: date,
+    title,
+    description,
+    image,
+    author,
+    category,
+    pitch,
+  } = post;
+
+  const parsedContent = md.render(pitch || "");
   return (
     <>
-      <h1 className="text-3xl">{post.title}</h1>
+      <section className="pink_container !min-h-[230px]">
+        <p className="tag">{formatDate(date)}</p>
+        <h1 className="heading">{title}</h1>
+        <p className="sub-heading !max-w-5xl">{description}</p>
+      </section>
+      <section className="section_container">
+        <img src={image} alt="thumbnail" className="w-full h-auto rounded-xl" />
+        <div className="space-y-5 mt-10 max-w-4xl mx-auto">
+          <div className="flex-between gap-5">
+            <Link
+              href={`/user/${author?._id}`}
+              className="flex gap-2 items-center mb-3 "
+            >
+              <Image
+                src={author?.image}
+                alt="avatar"
+                width={64}
+                height={64}
+                className="rounded-full drop-shadow-lg"
+              />
+              <div>
+                <p className="text-20-medium">{author?.name}</p>
+                <p className="text-16-medium !text-black-300">
+                  @{author?.username}
+                </p>
+              </div>
+            </Link>
+            <p className="category-tag">{category}</p>
+          </div>
+          <h3 className="text-30-bold">Pitch Details</h3>
+          {parsedContent ? (
+            <article
+              className="prose prose-neutral max-w-4xl font-work-sans break-all"
+              dangerouslySetInnerHTML={{ __html: parsedContent }}
+            />
+          ) : (
+            <p className="no-result"> No Details Provided</p>
+          )}
+        </div>
+        <hr className="divider" />
+
+        {/* Todo: Editor Selected Startups */}
+
+        <Suspense fallback={<Skeleton className="view_skeleton" />}>
+          <View id={id} />
+        </Suspense>
+      </section>
     </>
   );
 };
