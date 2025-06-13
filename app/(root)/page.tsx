4e62f087abf37aa78efcb4d3791fbf50b95eef77
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-13 05:42:11 +0300

    implement search functionality in startup queries and update data fetching in Home component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -9,9 +9,12 @@ export default async function Home({
   searchParams: Promise<{ query: string }>;
 }) {
   const query = (await searchParams).query;
+  const params = {
+    search: query || null,
+  };
 
   // const posts = await client.fetch(STARTUP_QUERIES);
-  const { data: posts } = await sanityFetch({ query: STARTUP_QUERIES });
+  const { data: posts } = await sanityFetch({ query: STARTUP_QUERIES, params });
 
   return (
     <>
