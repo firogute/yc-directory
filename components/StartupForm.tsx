9c85e7a4fb243b304be822520a8f0186b835d00b
Firomsa Guteta <firomsaguteta12@gmail.com>
2025-06-15 06:25:09 +0300

    integrate Sentry for error monitoring and reporting
    - Updated package.json to remove turbopack from dev script and added Sentry as a dependency.
    - Created a faulty API route for testing Sentry's error monitoring.
    - Implemented a global error handler to capture exceptions with Sentry.
    - Developed a frontend page to trigger errors and test Sentry integration.
    - Configured Sentry initialization for client, server, and edge environments.
    - Added error handling in the createPitch function with appropriate responses.

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -10,6 +10,7 @@ import { formSchema } from "@/lib/validation";
 import z from "zod";
 import { useToast } from "@/hooks/use-toast";
 import { useRouter } from "next/navigation";
+import { createPitch } from "@/lib/actions";
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({});
@@ -28,19 +29,19 @@ const StartupForm = () => {
       await formSchema.parseAsync(formValues);
       console.log(formValues);
 
-      // const result = await createIdea(prevState, formData, pitch);
-      // console.log(result);
+      const result = await createPitch(prevState, formData, pitch);
+      console.log(result);
 
-      // if (result.status == "SUCCESS") {
-      //   toast({
-      //     title: "Success",
-      //     description: "Your startup pitch has been created successfully",
-      //   });
-      // }
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created successfully",
+        });
+      }
 
-      // router.push(`/startup/${result.id}`);
+      router.push(`/startup/${result._id}`);
 
-      // return result;
+      return result;
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErrors = error.flatten().fieldErrors;
---
+++ sentry.edge.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry for edge features (middleware, edge routes, and so on).
+// The config you add here will be used whenever one of the edge features is loaded.
+// Note that this config is unrelated to the Vercel Edge Runtime and is also required when running locally.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://a81a244a913c2ed71b6dbc5506ca921d@o4509500383952896.ingest.de.sentry.io/4509500385394768",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
